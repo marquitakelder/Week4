dcb8b691679877555064dbe1b6cca950ab2bb276
marquitakelder <57427961+marquitakelder@users.noreply.github.com>
2020-10-13 15:59:18 -0400

    changes

---
+++ app1.js
@@ -0,0 +1,42 @@
+console.log("Hello World!");
+
+console.warn("This is a warning!");
+
+console.error("Error! System critical!");
+
+var var1;
+
+var1 = "Marquita";
+
+console.log(var1);
+
+console.log("\n");
+
+var2 = 7;
+
+console.log(var1 + var2); //Marquita7
+
+/* String = Strings are literal text
+Number = Numbers, in the console, are blue and should be written without quotes
+Boolean = True or False
+Undefined/Null/NaN = "Undefined" means a variable has been declared, but not assigned, "Null" is the absence of a value, and "NaN" stands for "Not a Number"
+Object = Objects are variables, but can contain many values
+Typeof = Finds the type of a variable */
+
+typeof "Marquita"; //String
+
+var var3 = "text";
+
+console.log(var3);
+
+var3 = true;
+
+console.log(var3);
+
+var1 = 5;
+
+var2 = "5";
+
+var2 = parseInt("5");
+
+console.log(var2 + var1);
